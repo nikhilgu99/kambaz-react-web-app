9e27f2e10f2f918352dd959a8a5a28b9d43d4611
Nikhil Gupta <99nikhilgupta@gmail.com>
2025-01-20 00:13:58 -0500

    Added name and section to lab 1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,7 +1,7 @@
 export default function Lab1() {
   return (
     <div id="wd-lab1">
-        <h2>Lab 1</h2>
+        <h2>Lab 1 - Nikhil Gupta - Section 01</h2>
 
         <h3>HTML Examples</h3>
         <div id="wd-h-tag">
