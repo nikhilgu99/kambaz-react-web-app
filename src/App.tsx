a53414550a432ff39328fcd0eabb17481ef8d5ad
Nikhil Gupta <99nikhilgupta@gmail.com>
2025-01-19 23:56:19 -0500

    Finished Assignment 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Labs from "./Labs";
+import Kambaz from "./Kambaz";
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -6,8 +7,9 @@ function App() {
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/" element={<Navigate to="Kambaz" />} />
           <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kambaz/*" element={<Kambaz />} />
         </Routes>
       </div>
     </HashRouter>
