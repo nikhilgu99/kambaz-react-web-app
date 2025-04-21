f0d707b504d00189b078fd7600ccdbb5d6c0e990
Nikhil Gupta <99nikhilgupta@gmail.com>
2025-04-20 23:31:13 -0400

    Added info to landing page

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -5,6 +5,7 @@ import AccountNavigation from "./Navigation";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Users from "./Users";
+import { Link } from "react-router";
 
 export default function Account() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
@@ -27,6 +28,11 @@ export default function Account() {
           </td>
         </tr>
       </table>
+      <div className="position-fixed bottom-0 end-0 p-1 text-end">
+        <p className="d-inline me-3">Nikhil Gupta - CS5610 Section 01</p>
+        <Link to={`https://github.com/nikhilgu99/kambaz-react-web-app/tree/project`} className="text-decoration-none text-dark me-3">[React UI Repo]</Link>
+        <Link to={`https://github.com/nikhilgu99/kambaz-node-server-app/tree/project`} className="text-decoration-none text-dark">[Node API Repo]</Link>
+      </div>
     </div>
   );
 }
